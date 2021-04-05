9b8b2d2cbd0882ea866c96743aefdf3f4a5960f5
Elvis Silva <elviscarlossouza@gmail.com>
2021-04-05 18:13:21 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import styles from '../css/main.css';
 
 const features = [
   {
